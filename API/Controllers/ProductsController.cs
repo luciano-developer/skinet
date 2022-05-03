93461ceb32017ec4c8e9bbedf1fa97f2a7b33b70
Luciano de Castro Oliveira <lucianooliveira@cnt.org.br>
2022-05-03 16:41:02 -0300

    Initial Create

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Core.Data;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext context;
+
+        public ProductsController(StoreContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index() {
+            var products = await context.Products?.ToListAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await context.Products.FindAsync(id);
+        }
+    }
+}
---
+++ Core/Data/Product.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Core.Data
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+    }
+}
